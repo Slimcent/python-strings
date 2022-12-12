e940ed849263f2cdaded974b64870f4bfa0ffbc6
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2022-12-12 12:20:31 +0100

    created a method for each string operation

--- main.py
+++ main.py
@@ -1,31 +1,44 @@
-name = 'Obichukwu'
-print("name is" + " " + name)
+def string_display():
+    name = 'Obichukwu'
+    print("name is" + " " + name)
 
-multi_line_string = """ Obinna is a good boy, fighting his
-darkness and demons all by himself,
-and hoping for a better tomorrow
-"""
-print(multi_line_string)
+    multi_line_string = """ Obinna is a good boy, fighting his
+    darkness and demons all by himself,
+    and hoping for a better tomorrow
+    """
+    print(multi_line_string)
 
-string_length = "Hello, Python"
-print(len(string_length))
 
-i = "Hello, Python"
-print(string_length[4])
+def string_length():
+    _length = "Hello, Python"
+    print(len(_length))
 
-for x in "banana":
-    print(x)
+    position = "Hello, Python"
+    print(position[4])
 
-check_string = "The best things in life are free!"
-print("free" in check_string)
 
-txt = "The best things in life are free!"
-if "free" in txt:
-    print("Yes, 'free' is present.")
+def string_loop():
+    for x in "banana":
+        print(x)
 
-txt1 = "The best things in life are free!"
-print("expensive" not in txt)
 
-txt2 = "The best things in life are free!"
-if "expensive" not in txt:
-    print("No, 'expensive' is NOT present.")
+def string_check():
+    check_string = "The best things in life are free!"
+    print("free" in check_string)
+
+    txt = "The best things in life are free!"
+    if "free" in txt:
+        print("Yes, 'free' is present.")
+
+    txt1 = "The best things in life are free!"
+    print("expensive" not in txt)
+
+    txt2 = "The best things in life are free!"
+    if "expensive" not in txt:
+        print("No, 'expensive' is NOT present.")
+
+
+string_display()
+string_length()
+string_check()
+string_loop()
