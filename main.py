fc76898bae13de86ca1a9005734de3b299c7b7d3
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2022-12-12 16:08:28 +0100

    created a methodfor string slice, split and format

--- main.py
+++ main.py
@@ -38,7 +38,42 @@ def string_check():
         print("No, 'expensive' is NOT present.")
 
 
+def string_slice():  # get characters from position 2 to position 5, excluding 5
+    text = "Hello, Python!"
+    print(text[2:5])
+
+    text1 = "Hello, Python!"  # get characters from the start position to position 5, excluding 5
+    print(text1[: 5])
+
+    text2 = "Hello, Python!"  # get characters from the position 2 to the end position
+    print(text2[2:])
+
+    text3 = "Hello, World!"  # - index means, get characters from the end of the string
+    print(text3[-5:-2])  # count 5 from the end, making the start position to be "o", and end position to be "l",
+    # ie -5[o], -4[r], -3[l]
+
+
+def string_split():
+    text = "Hello, World!"
+    print(text.split(","))  # returns ['Hello', ' World!']
+
+
+def string_format():
+    age = 60
+    txt = "My name is Obinna, and I am {}"
+    print(txt.format(age))
+
+    quantity = 3
+    item_no = 567
+    price = 49.95
+    my_order = "I want {} pieces of item {} for {} dollars."
+    print(my_order.format(quantity, item_no, price))
+
+
 string_display()
 string_length()
 string_check()
 string_loop()
+string_slice()
+string_split()
+string_format()
